eab4348359d3d02e99deeee04698c7f53752aad1
Connors Silverblue <co.alegria@gmail.com>
2025-11-24 18:47:25 -0300

    Actualizacion README agrego ejercicio practico  ejercicios de Modulo3-Lección 3

---
+++ Modulo 3/modulo3-leccion3-entrega/aps.js
@@ -0,0 +1,6 @@
+window.alert("A continuación deberá ingresar 3 notas por alumno para calcular el promedio final del curso. Las ponderaciones para la nota 1,nota 2 y nota 3 son respectivamente 25%, 35 y 40%");
+
+var cantidad=parseFloat(prompt("Ingrese cantidad de alumnos"));
+
+notasCurso=0
+for(let i =0;i<cantidad;i++){document}
